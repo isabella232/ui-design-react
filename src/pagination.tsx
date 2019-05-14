796bfd7e34c9f076d541333e2b0e36ed92b9980b
Chuck Masucci <cmasucci@gmail.com>
2019-05-14 10:57:27 -0700

    First commit

---
+++ src/pagination.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Pagination } from 'react-bootstrap';
+
+export interface Props {}
+
+export default class PaginationComponent extends React.Component {
+  public render() {
+    return (
+      <Pagination>
+        <Pagination.First />
+        <Pagination.Prev />
+        <Pagination.Item>{1}</Pagination.Item>
+        <Pagination.Ellipsis />
+
+        <Pagination.Item>{10}</Pagination.Item>
+        <Pagination.Item>{11}</Pagination.Item>
+        <Pagination.Item active>{12}</Pagination.Item>
+        <Pagination.Item>{13}</Pagination.Item>
+        <Pagination.Item disabled>{14}</Pagination.Item>
+
+        <Pagination.Ellipsis />
+        <Pagination.Item>{20}</Pagination.Item>
+        <Pagination.Next />
+        <Pagination.Last />
+      </Pagination>
+    );
+  }
+}

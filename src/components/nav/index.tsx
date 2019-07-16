1849428a4c143684e1111afbe8674e274ffa4f74
Chuck Masucci <cmasucci@gmail.com>
2019-07-16 08:09:11 -0700

    Navbar updates

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
-import { Nav } from 'react-bootstrap';
+import { Nav, NavProps, NavItem } from 'react-bootstrap';
 import NavItemDropdown from './nav-item-dropdown';
 import NavItemLink from './nav-item-link';
+import { NavLinkProps } from 'react-bootstrap/NavLink';
 
-export class NavComponent extends React.Component<any> {
-  public static Item = Nav.Item;
-  public static ItemDropdown = NavItemDropdown;
-  public static ItemLink = NavItemLink;
-  public static Link = Nav.Link;
-
-  public render() {
-    return <Nav {...this.props}>{this.props.children}</Nav>;
-  }
+function NavComponent(props: React.PropsWithChildren<NavProps>) {
+  return <Nav {...props}>{props.children}</Nav>;
 }
+
+NavComponent.Item = NavItem;
+NavComponent.ItemDropdown = NavItemDropdown;
+NavComponent.ItemLink = NavItemLink;
+NavComponent.Link = (props: React.PropsWithChildren<NavLinkProps>) => (
+  <Nav.Link {...props}>{props.children}</Nav.Link>
+);
 
 export default NavComponent;

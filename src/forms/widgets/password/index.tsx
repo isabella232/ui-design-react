6c96d29f8eb4d8904cfabb042b2471e86632555a
Chuck Masucci <cmasucci@gmail.com>
2019-10-15 11:05:45 -0700

    Removed old unused composition, updated to 0.1.0

--- src/forms/widgets/password/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { Props as InputProps } from '../../widgets/input';
-
-function PasswordWidget(props: InputProps) {
-  const { BaseInput } = props.registry.widgets;
-  return <BaseInput type="password" {...props} />;
-}
-
-export default PasswordWidget;
